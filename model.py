b85617d9a136e69f4c496ee0b12a7d30c73abdc1
Tiana Kutin <tiana.kutin@gmail.com>
2022-05-11 15:01:32 +0200

    nov razred Vislice

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ PONOVLJENA_CRKA = 'o'
 NAPACNA_CRKA = '-'
 ZMAGA = 'W'
 PORAZ = 'X'
+ZACETEK = 'S'
 
 class Igra:
     def __init__(self, geslo, crke=None):
@@ -74,3 +75,32 @@ import random
 def nova_igra():
     geslo = random.choice(bazen_besed)
     return Igra(geslo)
+
+class Vislice:
+    def __init__(self, igre):
+        self.igre = {}
+
+    def prost_id_igre(self):
+        return len(self.igre)
+
+        #n=0
+        #while n in self.igre:
+        #   n += 1
+        #return n
+
+        #import from uuid
+        #while True:
+        #kandidat = uuid.uuid4().int
+        #if kandidat not in self.igre:
+        #    return kandidat
+
+    def nova_igra(self):
+        igra = nova_igra()
+        novi_id = self.prost_id_igre()
+        self.igre[novi_id] = (igra, ZACETEK)
+        return novi_id
+
+    def ugibaj(self, id_igre, crka):
+        igra = self.igre[id_igre][0]
+        novo_stanje = igra.ugibaj(crka)
+        self.igre[id_igre] = (igra, novo_stanje)
